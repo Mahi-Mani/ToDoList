b1134d483c769ae8f90de2a157671ade2e12e32d
Mahisha Gunasekaran <m.mahishamani@gmail.com>
2020-02-14 09:53:33 -0800

    Updates

---
+++ precache-manifest.51dd94158104310027ee4a3559a1346f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0be87b0706de76de043772ba5c3fce1",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "4ef1b1df7d007c625ffd",
+    "url": "/ToDoList/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b394a67d26b939f657d2",
+    "url": "/ToDoList/static/js/2.b06194d3.chunk.js"
+  },
+  {
+    "revision": "46c482ef0575d001a3428c51daa7f5e8",
+    "url": "/ToDoList/static/js/2.b06194d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ef1b1df7d007c625ffd",
+    "url": "/ToDoList/static/js/main.c013a0eb.chunk.js"
+  },
+  {
+    "revision": "32cf6765e7dcfe1d6489",
+    "url": "/ToDoList/static/js/runtime-main.6c61717a.js"
+  },
+  {
+    "revision": "780db14599fe344f98f4b245b6ec6173",
+    "url": "/ToDoList/static/media/bg1.780db145.jpg"
+  }
+]);
